f4b1690523d40f084fd05fc8984e42679dd8fa35
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-03 16:09:49 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,17 +2,18 @@ class Project
   attr_accessor :backers
 
 
-initialize(title, backers)
-  @title = tile
-  @backers = []
+  def initialize(title, backers)
+    @title = tile
+    @backers = []
+  end
 
-def backers
-  @backers = backers
-end
+  def backers
+    @backers = backers
+  end
 
-def add_backer(backer)
-  @backers << backer
-  backer.packed_projects(project)
-end
+  def add_backer(backer)
+    @backers << backer
+    backer.packed_projects(project)
+  end
 
 end
