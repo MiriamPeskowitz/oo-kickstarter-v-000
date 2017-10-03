aa1269209055279d97c61046951114602218201b
Miriam Peskowitz <miriam5000@gmail.com>
2017-10-03 16:12:07 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,10 @@
 class Backer
-  attr_accessor :backed_projects
+  attr_accessor :backed_projects, :name
 
   def initialize(name)
     @name = name
     @backed_projects = []
   end
-
-
 
   def back_project(project)# shuld this be title?
     @backed_projects << project
